379c7f6db07cf5ebfc0128e92d75b76f7b783a25
jld3103 <jld3103yt@gmail.com>
2019-01-08 19:29:53 +0100

    Fixed/improved replacement plan filter

--- lib/ReplacementPlan/ReplacementPlanRow/ReplacementPlanRowWidget.dart
+++ lib/ReplacementPlan/ReplacementPlanRow/ReplacementPlanRowWidget.dart
@@ -12,6 +12,7 @@ class ReplacementPlanRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String nTeacher = change.teacher;
+    //print(change.lesson + nTeacher);
     String cTeacher = change.changed.teacher;
     bool showUnit = true;
     if (changes.indexOf(change) !=
