66e86a36a74f7f956d9ea7a12e0351e455cc4946
Finn <tupfi2@gmx.de>
2019-01-08 21:00:02 +0100

    Added Network file

---
+++ lib/Network.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+Future<bool> get checkServerConnection async {
+  try {
+    final result = await InternetAddress.lookup('api.vsa.2bad2c0.de');
+    if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+      return true;
+    }
+    return false;
+  } on SocketException catch (_) {
+    return false;
+  }
+}
+  
+Future<bool> get checkOnline async {
+  try {
+    final result = await InternetAddress.lookup('google.com');
+    if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+      return true;
+    }
+    return false;
+  } on SocketException catch (_) {
+    return false;
+  }
+}
