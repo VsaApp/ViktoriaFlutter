9e21e99a634671e96f6ea07e6105aedd0a4ceb73
Finn <tupfi2@gmx.de>
2019-01-22 18:06:29 +0100

    Speedup loading screen

--- lib/Loading/LoadingPage.dart
+++ lib/Loading/LoadingPage.dart
@@ -14,33 +14,48 @@ class LoadingPage extends StatefulWidget {
 }
 
 abstract class LoadingPageState extends State<LoadingPage> {
+ int countCurrentDownloads = 0; 
+ SharedPreferences instance;
+
   @override
   void initState() {
     SharedPreferences.getInstance().then((instance) {
-      downloadAll().then((_) {
-        // After download show app
-        WidgetsBinding.instance.addPostFrameCallback((_) async {
-          int storedVersion = instance.getInt(Keys.slidesVersion) ?? 0;
-          int currentVersion = 1;
-          if (currentVersion != storedVersion) {
-            instance.setInt(Keys.slidesVersion, currentVersion);
-            Navigator.of(context).pushReplacementNamed('/intro');
-          } else {
-            Navigator.of(context).pushReplacementNamed('/home');
-          }
-        });
-      });
+      this.instance = instance;
+      downloadAll();
     });
     super.initState();
   }
 
+  onFinishedAll() {
+    // After download show app
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      int storedVersion = instance.getInt(Keys.slidesVersion) ?? 0;
+      int currentVersion = 1;
+      if (currentVersion != storedVersion) {
+        instance.setInt(Keys.slidesVersion, currentVersion);
+        Navigator.of(context).pushReplacementNamed('/intro');
+      } else {
+        Navigator.of(context).pushReplacementNamed('/home');
+      }
+    });
+  }
+
   // Download all data
   Future downloadAll() async {
-    await UnitPlan.download();
-    await ReplacementPlan.download(
+    countCurrentDownloads = 4;
+    download(() async {
+      await UnitPlan.download();
+      await ReplacementPlan.download(
         (await SharedPreferences.getInstance()).getString(Keys.grade));
-    await WorkGroups.download();
-    await Calendar.download();
-    await Messageboard.download();
+    }, onFinishedAll);
+    download(WorkGroups.download, onFinishedAll); 
+    download(Calendar.download, onFinishedAll); 
+    download(Messageboard.download, onFinishedAll); 
+  }
+
+  Future download(Function() process, Function() onFinishedAll) async {
+    await process();
+    countCurrentDownloads--;
+    if (countCurrentDownloads == 0) onFinishedAll();
   }
 }
