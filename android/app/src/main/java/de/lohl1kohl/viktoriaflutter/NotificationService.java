58d29b30ee2ed6f0872069a56b9d23a8b0447e8c
jld3103 <jld3103yt@gmail.com>
2019-03-26 17:58:51 +0100

    Fixed notification vibration

--- android/app/src/main/java/de/lohl1kohl/viktoriaflutter/NotificationService.java
+++ android/app/src/main/java/de/lohl1kohl/viktoriaflutter/NotificationService.java
@@ -1,33 +1,24 @@
 package de.lohl1kohl.viktoriaflutter;
 
 import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
 import android.content.Context;
-import android.os.Build;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.NotificationChannel;
+import android.content.Intent;
 import android.graphics.Color;
-import android.os.Vibrator;
+import android.media.AudioManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
-import android.content.Intent;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-
-import org.json.JSONException;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Random;
 
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.view.FlutterView;
 
-public class NotificationService extends FirebaseMessagingService  {
+public class NotificationService extends FirebaseMessagingService {
     private static final String CHANNEL = "viktoriaflutter";
     static public FlutterView flutterView;
 
@@ -39,7 +30,7 @@ public class NotificationService extends FirebaseMessagingService  {
         System.out.println(remoteMessage.getNotification());
         System.out.println(remoteMessage.getData());
 
-        if (remoteMessage.getNotification() == null && remoteMessage.getData().get("notificationTitle") != null) {            
+        if (remoteMessage.getNotification() == null && remoteMessage.getData().get("notificationTitle") != null) {
             if (remoteMessage.getData().get("type").equals("replacementplan")) {
                 int changes = remoteMessage.getData().get("notificationBody").length() - remoteMessage.getData().get("notificationBody").replace("\n", "").length() + 1;
                 showNotification(
@@ -53,12 +44,12 @@ public class NotificationService extends FirebaseMessagingService  {
                 System.out.println("notify");
             } else {
                 showNotification(
-                    remoteMessage.getData().get("notificationTitle"),
-                    remoteMessage.getData().get("notificationBody"),
-                    remoteMessage.getData().get("notificationBody"),
-                    remoteMessage.getData().get("type") +  "_channel",
-                    (new Random().nextInt() * 10000 + 5),
-                    remoteMessage.getData()
+                        remoteMessage.getData().get("notificationTitle"),
+                        remoteMessage.getData().get("notificationBody"),
+                        remoteMessage.getData().get("notificationBody"),
+                        remoteMessage.getData().get("type") + "_channel",
+                        (new Random().nextInt() * 10000 + 5),
+                        remoteMessage.getData()
                 );
             }
         } else if (remoteMessage.getNotification() == null) {
@@ -73,14 +64,12 @@ public class NotificationService extends FirebaseMessagingService  {
 
     public void showNotification(String title, String body, String bodyLong, String channelId, int notificationId, Map<String, String> data) {
 
+        AudioManager am = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("channel", channelId);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-        // Vibrate for 500 milliseconds
-        v.vibrate(500);
 
         NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext(), channelId)
                 .setContentTitle(title)
@@ -90,11 +79,18 @@ public class NotificationService extends FirebaseMessagingService  {
                 .setStyle(new NotificationCompat.BigTextStyle().bigText(bodyLong))
                 .setContentIntent(pendingIntent)
                 //.setSound()
-                .setVibrate(new long[]{500, 500})
                 .setColor(Color.parseColor("#ff5bc638"))
                 .setGroup("group" + Integer.toString(notificationId))
                 .setAutoCancel(true)
                 .setColorized(true);
+
+        if (am.getRingerMode() != AudioManager.RINGER_MODE_SILENT) {
+            System.out.println("Vibrate");
+            notification.setVibrate(new long[]{500, 500});
+        } else {
+            System.out.println("Silent");
+            notification.setVibrate(new long[]{0});
+        }
 
         NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
         manager.notify(notificationId, notification.build());
